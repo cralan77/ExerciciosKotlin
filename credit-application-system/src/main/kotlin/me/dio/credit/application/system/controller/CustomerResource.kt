ede4672cdfa0fc9e3eef0684069f2696f9abf4f7
alan <cr.alan9119@gmail.com>
2024-12-18 23:19:45 -0300

    Finish CustomerResource

--- credit-application-system/src/main/kotlin/me/dio/credit/application/system/controller/CustomerResource.kt
+++ credit-application-system/src/main/kotlin/me/dio/credit/application/system/controller/CustomerResource.kt
@@ -1,10 +1,18 @@
 package me.dio.credit.application.system.controller
 
+import me.dio.credit.application.system.dto.CustomerView
 import me.dio.credit.application.system.dto.CustomerDto
+import me.dio.credit.application.system.dto.CustomerUpdateDto
+import me.dio.credit.application.system.entity.Customer
 import me.dio.credit.application.system.service.impl.CustomerService
+import org.springframework.web.bind.annotation.DeleteMapping
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PatchMapping
+import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
@@ -17,5 +25,23 @@ class CustomerResource (
     fun saveCustomer(@RequestBody customerDto: CustomerDto): String {
         val savedCustomer = this.customerService.save(customerDto.toEntity())
         return "Customer ${savedCustomer.email} saved!"
+    }
+
+    @GetMapping("/{id}")
+    fun findById(@PathVariable id: Long): CustomerView {
+        val customer : Customer = this.customerService.findById(id)
+        return CustomerView(customer)
+    }
+
+    @DeleteMapping("/{id}")
+    fun deleteCustomer(@PathVariable id: Long) = this.customerService.delete(id)
+
+    @PatchMapping
+    fun updateCustomer(@RequestParam(value = "customerId") id: Long,
+                       @RequestBody customerUpdateDto: CustomerUpdateDto): CustomerView {
+        val customer: Customer = this.customerService.findById(id)
+        val customerToUpdate: Customer = customerUpdateDto.toEntity(customer)
+        val customerUpdated: Customer = customerService.save(customerToUpdate)
+        return CustomerView(customerUpdated)
     }
 }
